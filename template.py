352061ab5a28fe5f24ca8119c84ead6f2243419b
Jayasuryan Mutyala <jsm.dgme@gmail.com>
2026-03-15 11:31:54 +0530

    project template setup

---
+++ template.py
@@ -0,0 +1,54 @@
+import os 
+from pathlib import Path
+import logging 
+
+logging.basicConfig(level=logging.INFO,
+                    format="[%(asctime)s]: %(message)s")
+
+project_name = "src"
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/agents/__init__.py",
+    f"{project_name}/agents/orchestrator.py",
+    f"{project_name}/agents/transciptor_agent.py",
+    f"{project_name}/agents/rag_agent.py",
+    f"{project_name}/agents/mcp_agent.py",
+    f"{project_name}/tools/__init__.py",
+    f"{project_name}/tools/youtube_tools.py",
+    f"{project_name}/tools/retrieval_tools.py",
+    f"{project_name}/state/__init__.py",
+    f"{project_name}/state/graph_state.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/logger/custom_logger.py",
+    f"{project_name}/exceptions/__init__.py",
+    f"{project_name}/exceptions/custom_exception.py",
+    "backend/api/__init__.py",
+    "backend/api/main.py",
+    "backend/api/routers/__init__.py",
+    "backend/api/routers/summarize.py",
+    "backend/api/routers/qa.py",
+    "frontend/__init__.py",
+    "frontend/streamlit_app.py",
+    "mcp_config/servers.json",
+    ".env",
+    "requirements.txt",
+]
+
+for file_path in list_of_files:
+    file_path =  Path(file_path)
+    file_dir,file_name = os.path.split(file_path)
+
+    if file_dir != "":
+        os.makedirs(file_dir,exist_ok=True)
+        logging.info(f"Creating directory: {file_dir} for file: {file_name}")
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path,"w") as f:
+            pass
+            logging.info(f"Creating an empty file: {file_path}")
+    
+    else:
+        logging.info(f"{file_name} already exists")
+
+
